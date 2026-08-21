e6318bccc73543952b4f27a215ca46cfb2946808
Samuli Vahtera <samuli.vahtera@proton.me>
2026-08-21 22:19:34 +0300

    Lisää tehtävä 3

--- tehtavat/3-4.py
+++ tehtavat/3-4.py
@@ -1,19 +1,16 @@
 #Lue luvut
 import math
 
-firstStr=input("Syötä ensimmäinen kokonaisluku: ")
-secondStr=input("Syötä toinen kokonaisluku: ")
-thirdStr=input("Syötä kolmas kokonaisluku: ")
-first=int(firstStr)
-second=int(secondStr)
-third=int(thirdStr)
+first=int(input("Syötä ensimmäinen kokonaisluku: "))
+second=int(input("Syötä toinen kokonaisluku: "))
+third=int(input("Syötä kolmas kokonaisluku: "))
 
 #Laskut
-sum=first+second+third
+sum=sum((first,second,third))
 product=first*second*third
 average=sum/3
 
 #Tulostus
 print(f"{'Lukujen summa on '}{sum}{'.'}")
 print(f"{'Lukujen tulo on '}{product}{'.'}")
-print(f"{'Lukujen keskiarvo on '}{average}{'.'}")
+print(f"{'Lukujen keskiarvo on '}{average:.2f}{'.'}")
