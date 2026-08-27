8c852ddf84e3f4dd59c5bb779278dbf28dd71dea
Samuli Vahtera <samuli.vahtera@proton.me>
2026-08-27 14:30:36 +0300

    teht. 8 jatkuu

--- mod08/8-3.py
+++ mod08/8-3.py
@@ -1,20 +1,21 @@
 dictAirports={}
 sExitMsg="Kiitos Käynnistä"
-sQuery=""
+sEntry=""
 
 def newAirport ():
     sNameIn=input("Lentoaseman nimi: ")
     sCodeIn=str.capitalize(input("ICAO: "))
     dictAirports[sNameIn]=sCodeIn
     
-while sQuery!="L" or sQuery!="U":
-    sQuery=str.capitalize(input("(U)usi vai (L)opeta? "))
-while sQuery!="L" or sQuery!="U" or sQuery!="H":
-    if sQuery=="L":
+#while sEntry!="L" or sQuery!="U":
+sEntry=str.capitalize(input("(U)usi vai (L)opeta? "))
+while sEntry!="L" or sEntry!="U" or sEntry!="H":
+    if sEntry=="L":
         exit(sExitMsg)
-    elif sQuery=="U":
+    elif sEntry=="U":
         newAirport()
-        print(dictAirports)
-    elif sQuery=="H":
-        pass
-    sQuery=str.capitalize(input("(U)usi, (H)aku vai (L)opeta? "))
+    elif sEntry=="H":
+        sQuery=input("Hae lentokenttä: ")
+        if sQuery in dictAirports:
+            print(f"{sQuery}: {dictAirports[sQuery]}")
+    sEntry=str.capitalize(input("(U)usi, (H)aku vai (L)opeta? "))
