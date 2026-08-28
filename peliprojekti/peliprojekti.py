cc67a7a0905b38c6a13c3f33036a6e76e27eb0e1
Samuli Vahtera <samuli.vahtera@proton.me>
2026-08-28 18:03:59 +0300

    lisää projektia

--- peliprojekti/peliprojekti.py
+++ peliprojekti/peliprojekti.py
@@ -9,14 +9,16 @@ else:
     print(f"Terve {playerName}! Ikäsi on {playerAge}.\n")
 
 #mainMenu
-while str.upper(playerInput)!="LOPETA":
-    print("HIENO OTSIKKO\n")
-    print(f"1. Aloita\n2. Epäsulje hypervalikko\n3. Siisteydentunnistus\n tai LOPETA")   #Valikoiden status puuttuu
-    playerInput=input("")
-    if playerInput==1:
-        break
-    if playerInput==2:
+while str.upper(playerInput)!="1":
+    print("\nHIENO OTSIKKO\n")
+    print(f"1. Aloita\n2. Epäsulje hypervalikko\n3. Siisteydentunnistus\n4. Luut\n\ntai LOPETA")   #Valikoiden status puuttuu
+    playerInput=input()
+    if playerInput=="2":
         print("Hypervalikko epäsuljettu.")
-    if playerInput==3:
-        print("Siisteyden tunnistus aktivoitu")
+    if playerInput=="3":
+        print("Siisteyden tunnistus aktivoitu.")
+    if playerInput=="4":
+        print("Luut pois päältä.")
+    if str.upper(playerInput)=="LOPETA":
+        exit("Kiitos käynnistä")
 print("Ei implementoitu")
